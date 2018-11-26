07b15d7150277222973ae5056f071cd30ed473a3
Sibi Prabakaran <sibi@psibi.in>
2018-11-26 17:48:33 +0530

    Push the minimal working tests as of now

---
+++ unliftio/test/UnliftIO/PooledAsyncSpec.hs
@@ -0,0 +1,26 @@
+module UnliftIO.PooledAsyncSpec (spec) where
+
+import Test.Hspec
+import Control.Concurrent
+import Data.List (nub)
+import UnliftIO
+
+spec :: Spec
+spec = do
+  let exAction :: Int -> IO Int
+      exAction x = do
+        if (x == 2) then error "hell" else return ()
+        return x
+               
+      action :: Int -> IO ThreadId
+      action x = do
+         threadDelay (2 * 10^6)
+         myThreadId
+                 
+  describe "pooled mapConcurrency" $ do
+    it "Throws exception properly" $ do
+       (pooledMapConcurrently 5 exAction [1..5]) `shouldThrow` anyErrorCall
+    
+    it "spawns five threads" $ do
+       xs <- (pooledMapConcurrently 5 action [1..5])
+       (length $ nub xs) `shouldBe` 5
